84c28656aee4ba04cdfcd78d92bb396a07166130
wj <1076813536@qq.com>
2021-07-03 23:04:19 +0800

    Mytest.java

--- app01/src/com/gitdemo/web/servlet/Mytest.java
+++ app01/src/com/gitdemo/web/servlet/Mytest.java
@@ -1,8 +1,10 @@
 package com.gitdemo.web.servlet;
 
+import java.util.HashMap;
+
 /**
  * @ClassName: Mytest
- * @Description: TODO
+ * @Description: HashMap
  * @Author wj
  * @Date 2021/7/3
  * @Version 1.0
@@ -10,5 +12,13 @@ package com.gitdemo.web.servlet;
 public class Mytest {
     public static void main(String[] args) {
         System.out.println("-------my test------");
+        HashMap<String,Object> map = new HashMap();
+        System.out.println(map);                             // {}
+        System.out.println(map.put("通话",10));               // null
+        System.out.println(map.put("随便",20));               // null
+        System.out.println(map.put("通话",30));               // 10
+        System.out.println(map.put("重地",40));               // null
+        System.out.println("集合中键值对数量：" + map.size());   // 集合中键值对数量：3
+        System.out.println("集合内容" + map);                  // 集合内容{通话=30, 随便=20, 重地=40}
     }
 }
