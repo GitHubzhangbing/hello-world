9e54469e7ed693417103b2df137e794b4afd1994
GitHubzhangbing <1076813536@qq.com>
2021-07-03 18:39:35 +0800

    Mytest.java

---
+++ app01/src/com/gitdemo/web/servlet/Mytest.java
@@ -0,0 +1,14 @@
+package com.gitdemo.web.servlet;
+
+/**
+ * @ClassName: Mytest
+ * @Description: TODO
+ * @Author wj
+ * @Date 2021/7/3
+ * @Version 1.0
+ */
+public class Mytest {
+    public static void main(String[] args) {
+        System.out.println("-------my test------");
+    }
+}
